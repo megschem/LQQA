4a2b706f54ec0c961efb8772f10f363bfa545b89
megschem <megschem@gmail.com>
2019-08-12 13:29:50 -0500

    set up React Router paths

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.scss';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -12,23 +13,24 @@ import Privacy from './components/Privacy';
 import Terms from './components/Terms';
 import Support from './components/Support';
 
-class App extends React.Component {
-  render() {
+function App() {
     return(
-      <div className="container home-page">
-        <Header />
-        <Home />
-        {/* <Search /> */}
-        {/* <ResultsList /> */}
-        {/* <Result /> */}
-        {/* <SubmitQuestion /> */}
-        {/* <About /> */}
-        {/* <Terms /> */}
-        {/* <Privacy /> */}
-        {/* <Support /> */}
-      </div>
+      <Router>
+        <div className="container home-page">
+          <Header />
+
+          <Route path="/" exact component={Home} />
+          <Route path="/search/" component={Search} />
+          <Route path="/search-results/" component={ResultsList} />
+          <Route path="/result/" component={Result} />
+          <Route path="/submit-question/" component={SubmitQuestion} />
+          <Route path="/about/" component={About} />
+          <Route path="/terms-of-service/" component={Terms} />
+          <Route path="/privacy-policy/" component={Privacy} />
+          <Route path="/support/" component={Support} />
+        </div>
+      </Router>
     );
-  }
 }
 
 const mountNode = document.getElementById('app');
